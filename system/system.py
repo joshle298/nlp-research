a1c48f67e269075df3cd5812419ee1a9b34a4f35
Alan Chen <azc242@nyu.edu>
2023-04-30 14:25:41 -0400

    read data into dataframe and drop extra column from tsv

--- system/system.py
+++ system/system.py
@@ -6,11 +6,14 @@ import pandas as pd
 
 # Read tab-separated file with column names
 df = pd.read_csv(
-    r"data\ubereats_data.tsv",
+    r"../data\ubereats_data.tsv",
+    encoding="utf-8",
     sep="\t",
     lineterminator="\n",
-    names=["kitchenType", "name", "address", "menu"],
+    index_col=False,
+    names=["kitchenType", "name", "address", "delete", "menu"],
 )
+df = df.drop(columns="delete", axis=1)
 
 # Print the first five rows to verify the data has been loaded correctly
 print(df.head())
